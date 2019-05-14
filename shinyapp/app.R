8d26db1829060f2c1e2022a1c6a0294047db8def
Kevin Gartland <kgartland@rstudio.com>
2019-05-14 12:11:10 -0400

    first deploy

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Deploy #1"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
