5422710c97fb2d501de247c361db75a49e0ad305
Kevin Gartland <kgartland@rstudio.com>
2019-11-19 10:03:08 -0500

    new test

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Deploy rewind"),
+   titlePanel("fastforward"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
