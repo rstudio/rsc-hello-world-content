e1fc5c9c233e97c7d30bfc2085167fa8ea704b1a
Kevin Gartland <kgartland@rstudio.com>
2019-11-19 09:46:33 -0500

    rewind

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Deploy #2"),
+   titlePanel("Deploy rewind"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
