18bef1bdfa61c8121feb4fe03835a485ac3c8eea
Kevin Gartland <kgartland@rstudio.com>
2020-06-11 14:50:20 -0400

    new

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data 4"),
+   titlePanel("Old Faithful Geyser Data 5"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
