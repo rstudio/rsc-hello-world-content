31efe3d87d3cb7b7d642944427407ca79dad798a
Kevin Gartland <kgartland@rstudio.com>
2020-06-12 12:23:25 -0400

    new hash

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("no 0"),
+   titlePanel("no 0 "),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
