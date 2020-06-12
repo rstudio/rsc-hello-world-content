4efa63ddc8dcefc823d21ec2c1f668f0f127c6c1
Kevin Gartland <kgartland@rstudio.com>
2020-06-12 12:26:34 -0400

    orig hash

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("no 0 "),
+   titlePanel("orig hash"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
