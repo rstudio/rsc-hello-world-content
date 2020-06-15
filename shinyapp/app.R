d5265b8810bada4e4f1c96b35e6d538acc7a8dcb
Kevin Gartland <kgartland@rstudio.com>
2020-06-15 15:52:51 -0400

    new

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Gremlins 2 blah blah 9 blah"),
+   titlePanel("Gremlins 2"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
