cc37dd4e35409cd26e57f26870d901deb45a7345
Kevin Gartland <kgartland@rstudio.com>
2020-06-15 12:38:05 -0400

    new batch

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Gremlins"),
+   titlePanel("Gremlins 2: The New Batch"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
