0be58efe5aa440805129867668dac3d6ca5c370c
Kevin Gartland <kgartland@rstudio.com>
2020-06-15 16:08:59 -0400

    new

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -21,8 +21,8 @@ ui <- shinyUI(fluidPage(
          sliderInput("bins",
                      "Number of bins:",
                      min = 1,
-                     max = 50,
-                     value = 30)
+                     max = 100,
+                     value = 56)
       ),
       
       # Show a plot of the generated distribution
