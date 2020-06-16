07d825af39c4ca6f9fc9ee27a1aa1d1121a1d78f
Kevin Gartland <kgartland@rstudio.com>
2020-06-16 10:01:32 -0400

    slow

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Gremlins 9-noman-2test blah"),
+   titlePanel("Gremlins 9-noman-2test blah slow"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
