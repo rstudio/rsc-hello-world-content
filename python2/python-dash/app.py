60e877d1bbde1ffa0c2a71563f802ffae22d5877
Kevin Gartland <kgartland@rstudio.com>
2022-03-24 11:22:17 -0400

    force update

--- python2/python-dash/app.py
+++ python2/python-dash/app.py
@@ -3,6 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+#force update
+
 import pandas as pd
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
