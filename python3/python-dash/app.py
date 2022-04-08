c925b0256f42e9bd6cd53608bcc2653522713b41
Kevin Gartland <kgartland@rstudio.com>
2022-04-08 10:52:14 -0400

    update

--- python3/python-dash/app.py
+++ python3/python-dash/app.py
@@ -2,7 +2,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-blue
 import pandas as pd
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
