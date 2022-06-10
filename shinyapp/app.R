c7755a5e809daf4f39ed2794c5071dfd85816e1d
nkakade-rstudio <namrata.kakade@rstudio.com>
2022-06-10 15:18:25 -0700

    Make some updates to the application 3

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -13,16 +13,16 @@ library(shiny)
 ui <- shinyUI(fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data - june 10 update 2"),
+   titlePanel("Old Faithful Geyser Data - june 10 update 3"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          sliderInput("bins",
-                     "Number of bins - june 10 update 2:",
+                     "Number of bins - june 10 update 3:",
                      min = 1,
-                     max = 10,
-                     value = 5)
+                     max = 5,
+                     value = 8)
       ),
       
       # Show a plot of the generated distribution
@@ -41,7 +41,7 @@ server <- shinyServer(function(input, output) {
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'blue', border = 'green')
+      hist(x, breaks = bins, col = 'yellow', border = 'blue')
    })
 })
 
