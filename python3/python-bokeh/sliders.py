aec22c80af5dd96587e3ac90256f3758f981f5cd
Kevin Gartland <kgartland@rstudio.com>
2023-10-18 10:01:24 -0400

    test

--- python3/python-bokeh/sliders.py
+++ python3/python-bokeh/sliders.py
@@ -21,7 +21,7 @@ from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider, TextInput
 from bokeh.plotting import figure
 import pandas
-print("update5 9:51AM")
+print("update6 10:00AM")
 # Set up data
 N = 200
 x = np.linspace(0, 4 * np.pi, N)
