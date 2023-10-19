f29b4bfa4b7c0ff1a5a5a66f17808f4b8d0bea6a
Kevin Gartland <kgartland@rstudio.com>
2023-10-19 14:00:16 -0400

    fresh

--- python3/python-bokeh/sliders.py
+++ python3/python-bokeh/sliders.py
@@ -21,7 +21,7 @@ from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider, TextInput
 from bokeh.plotting import figure
 import pandas
-print("update 1:58")
+print("update 2:00")
 
 # Set up data
 N = 200
